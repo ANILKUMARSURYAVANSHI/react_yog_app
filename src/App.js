248f18d1543e4716b7130b4a32b06322f698c62d
anil suryavanshi <thakurinfotech@gmail.com>
2018-02-23 20:56:26 +0530

    Correction in State

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Yog from './Yog/yog';
 
 class App extends Component {
-  states = {
+  state = {
     yogs: [{
       name: 'Asan'
     },
@@ -15,7 +15,7 @@ class App extends Component {
 
   someHandler = (newval) => {
 
-    console.log('11')
+    console.log('11');
     this.setState({
       yogs: [
         { name: newval },
@@ -41,13 +41,17 @@ class App extends Component {
 
 
   render() {
+    const style = {
+      backgroundColor: 'red',
+      border: '1px solid blue'
+    }
     return (
-
+       
       <div>
-        <button onClick={this.someHandler.bind(this, 'Samathi')} >Switch Yog</button>
-        <p>{this.states.yogs[0].name}</p>
-        <Yog click={this.someHandler.bind(this,'Dhyan')} name={this.states.yogs[0].name} />
-        <Yog changed={this.someNameHandler} name={this.states.yogs[1].name} >  New Yog</Yog>
+        <button style={style} onClick={this.someHandler.bind(this, 'Samathi')} >Switch Yog</button>
+        <p >{this.state.yogs[0].name}</p>
+        <Yog click={this.someHandler.bind(this,'Dhyan')} name={this.state.yogs[0].name} />
+        <Yog changed={this.someNameHandler} name={this.state.yogs[1].name} >  New Yog</Yog>
       </div>
 
       //React.createElement('div',null,React.createElement('h1',{className:'App'},'anil'))
