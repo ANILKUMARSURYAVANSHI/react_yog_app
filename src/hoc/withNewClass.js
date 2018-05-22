99ca662a4b3bf0691c16719de3315b57000f36a4
anil suryavanshi <thakurinfotech@gmail.com>
2018-05-22 20:33:33 +0530

    Passed Unknown props as well as

--- src/hoc/withNewClass.js
+++ src/hoc/withNewClass.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 const withNewClass = (WrappedComponent, className)=>{
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent  {...props} />
+    // return (props) => (
+    //     <div className={className}>
+    //         <WrappedComponent  {...props} />
+    //
+    //     </div>
+    // )
 
-        </div>
-    )
+    return class extends Component {
+        render(){
+            return (
+                <div className={className}>
+                           <WrappedComponent  {...this.props} />
+
+                       </div>
+            )
+        }
+    }
 }
 
 export default  withNewClass;
