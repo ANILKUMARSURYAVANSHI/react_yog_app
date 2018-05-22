786eada6ddd5f72ad8f2b3cd7b4acff758178286
anil suryavanshi <thakurinfotech@gmail.com>
2018-05-22 20:28:19 +0530

    Truly Working HOC

--- src/hoc/withNewClass.js
+++ src/hoc/withNewClass.js
@@ -3,7 +3,7 @@ import React from 'react';
 const withNewClass = (WrappedComponent, className)=>{
     return (props) => (
         <div className={className}>
-            <WrappedComponent/>
+            <WrappedComponent  {...props} />
 
         </div>
     )
